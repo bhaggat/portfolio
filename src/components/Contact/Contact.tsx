9bd852b795e2566d288aad8de877a7b2019c85d8
Dhruv <dhruvbhagat68@gmail.com>
2024-12-03 18:36:54 +0530

    added subtitle back

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -57,10 +57,10 @@ function Contact() {
       <div className="items-container">
         <div className="contact_wrapper">
           <h1>Contact Me</h1>
-          {/* <p>
+          <p>
             Got a project waiting to be realized? Let's collaborate and make it
             happen!
-          </p> */}
+          </p>
           <Box
             ref={form}
             component="form"
